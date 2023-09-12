a146ec10c5052fbb05376d9f9292aed596125838
sanctus patricius <patriciussanctus@gmail.com>
2023-09-13 01:32:30 +0300

    markup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="form">
+      <div>
+        <input v-model="form.price" placeholder="Цена">
+        <br>
+        <label>{{ form.price }}</label>
+      </div>
+      <div>
+        <input v-model="form.qty" placeholder="Кол-во">
+        <br>
+        <label>{{ form.qty }}</label>
+      </div>
+      <div>
+        <input v-model="form.amount" placeholder="Сумма">
+        <br>
+        <label>{{ form.amount }}</label>
+      </div>
+      <div>
+        <button @click="submit">submit</button>
+        <br>
+        <label>{{ JSON.stringify(localStorage) }}</label>
+      </div>
+    </div>
+    <div class="logs">
+      <p v-for="(e, k) in eventLogs" :key="k">{{ JSON.stringify(e) }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    localStorage: localStorage,
+    eventLogs: [],
+    form: {
+      nonce: 0,
+      price: null,
+      qty: null,
+      amount: null
+    }
+  }),
+  methods: {
+    submit () {
+
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  #app
+    font-family: Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+    .form
+      display: flex
+      justify-content: space-around
+    .logs
+      height: 80vh
+      width: 100%
+      background-color: #eee
 </style>
